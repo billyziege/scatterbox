605a0789f1ab2a779e68f09bcb237bdea63cf80a
billyziege <brandon.s.zerbe@gmail.com>
2016-05-17 14:27:27 -0400

    Created a sub-directory and a setup script for installing locally.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scatterbox',
+      version='0.2',
+      description='Creates a vpython simulation for multi-particle diffusion.',
+      url='',
+      author='Gerd and Brandon Zerbe',
+      author_email='zerbe@msu.edu',
+      license='GPL',
+      packages=['scatterbox'],
+      zip_safe=False)
